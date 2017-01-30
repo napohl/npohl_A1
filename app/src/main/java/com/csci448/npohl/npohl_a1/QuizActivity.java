2d9fda56c982011aeb86e2b6fc5ebd13a3fff649
Nathan <npohl@mymail.mines.edu>
2017-01-29 20:23:16 -0700

    Upgraded view to update depending on the question type. There is a bug that causes the app to crash when switching to landscape, and the checkAnswer functions still need to be fixed.

--- app/src/main/java/com/csci448/npohl/npohl_a1/QuizActivity.java
+++ app/src/main/java/com/csci448/npohl/npohl_a1/QuizActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,7 +22,13 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPrevButton;
+    private Button mAButton;
+    private Button mBButton;
+    private Button mCButton;
+    private Button mDButton;
+    private Button mSubmitButton;
     private TextView mQuestionTextView;
+    private EditText mFreeResponseAnswer;
 
     private Question[] mQuestionBank = new Question[] {
         new Question(R.string.question_americas, R.string.true_button, TF),
@@ -33,15 +40,65 @@ public class QuizActivity extends AppCompatActivity {
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
-    mQuestionTextView.setText(question);
-}
+        mQuestionTextView.setText(question);
 
-    private void checkAnswer(boolean userPressedTrue) {
-        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        QuestionType type = mQuestionBank[mCurrentIndex].getQuestionType();
+
+        if (type == TF) {
+            mTrueButton.setVisibility(View.VISIBLE);
+            mFalseButton.setVisibility(View.VISIBLE);
+            mAButton.setVisibility(View.GONE);
+            mBButton.setVisibility(View.GONE);
+            mCButton.setVisibility(View.GONE);
+            mDButton.setVisibility(View.GONE);
+            mFreeResponseAnswer.setVisibility(View.GONE);
+            mFreeResponseAnswer.setText("");
+            mSubmitButton.setVisibility(View.GONE);
+        }
+
+        else if (type == MC) {
+            mTrueButton.setVisibility(View.GONE);
+            mFalseButton.setVisibility(View.GONE);
+            mAButton.setVisibility(View.VISIBLE);
+            mBButton.setVisibility(View.VISIBLE);
+            mCButton.setVisibility(View.VISIBLE);
+            mDButton.setVisibility(View.VISIBLE);
+            mFreeResponseAnswer.setVisibility(View.GONE);
+            mFreeResponseAnswer.setText("");
+            mSubmitButton.setVisibility(View.GONE);
+        }
+
+        else if (type == FR) {
+            mTrueButton.setVisibility(View.GONE);
+            mFalseButton.setVisibility(View.GONE);
+            mAButton.setVisibility(View.GONE);
+            mBButton.setVisibility(View.GONE);
+            mCButton.setVisibility(View.GONE);
+            mDButton.setVisibility(View.GONE);
+            mFreeResponseAnswer.setVisibility(View.VISIBLE);
+            mFreeResponseAnswer.setText("");
+            mSubmitButton.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    /**
+     * This checkAnswer checks for the correct answer of both TF and MC types
+     *
+     * checkAnswer(int) is used when the TF and MC types of questions are asked. It is called
+     * whenever the true, false, a, b, c, and d buttons are hit. It takes the text from the button
+     * and checks it against the string for the answer. TF uses the true_button and false_button
+     * text, while the MC uses the text on its respective buttons.
+     *
+     * @param buttonResId the id of the button that has been clicked
+     */
+    private void checkAnswer(int buttonResId) {
+
+        int answer = mQuestionBank[mCurrentIndex].getAnswerResId();
 
         int messageResId = 0;
 
-        if (userPressedTrue == answerIsTrue) {
+        if (answer == buttonResId) {
             messageResId = R.string.correct_toast;
         }
         else {
@@ -49,6 +106,23 @@ public class QuizActivity extends AppCompatActivity {
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void checkAnswer(String userAnswer) {
+
+        int answer = mQuestionBank[mCurrentIndex].getAnswerResId();
+
+        int messageResId = 0;
+        /*
+        TODO: Check how to access the string refrenced by the res id
+         */
+        if (true) {
+            messageResId = R.string.correct_toast;
+        }
+        else {
+            messageResId = R.string.incorrect_toast;
+        }
+
     }
 
     @Override
@@ -66,18 +140,21 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        mFreeResponseAnswer = (EditText) findViewById(R.id.fr_answer);
+
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkAnswer(true);
+                checkAnswer(R.id.true_button);
             }
         });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkAnswer(false);
+                checkAnswer(R.id.false_button);
             }
         });
 
@@ -99,6 +176,46 @@ public class QuizActivity extends AppCompatActivity {
                     mCurrentIndex = mQuestionBank.length - 1;
                 }
                 updateQuestion();
+            }
+        });
+
+        mAButton = (Button) findViewById(R.id.a_button);
+        mAButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(R.id.a_button);
+            }
+        });
+
+        mBButton = (Button) findViewById(R.id.b_button);
+        mBButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(R.id.b_button);
+            }
+        });
+
+        mCButton = (Button) findViewById(R.id.c_button);
+        mCButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(R.id.c_button);
+            }
+        });
+
+        mDButton = (Button) findViewById(R.id.d_button);
+        mDButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(R.id.d_button);
+            }
+        });
+
+        mSubmitButton = (Button) findViewById(R.id.submit_button);
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(R.id.submit_button);
             }
         });
 
